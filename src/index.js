3ca471a1d39e56a7bf499bdb5452a2475d5b06ac
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-09 21:15:32 +0200

    Continued Work

--- src/index.js
+++ src/index.js
@@ -10,14 +10,15 @@ const App = () => {
   const showCountries = countryArray =>
     countryArray.map(country => (
       <li key={country.numericCode} className="countryLi">
+        <img src={country.flag} alt="" width="30" />
+        <p className="countrName">{country.name}</p>
         <button
           className="btn"
           value={country.name}
           onClick={onSearchValueChanged}
         >
           Show
-        </button>{" "}
-        {country.name}
+        </button>
       </li>
     ));
 
@@ -40,13 +41,15 @@ const App = () => {
   const showCountryData = () => {
     if (visibleCountries.length === 1) {
       return (
-        <div>
+        <div className="showCountry">
           <h1>{visibleCountries[0].name}</h1>
-          <h4>
-            Population:{" "}
-            {Math.round(visibleCountries[0].population / 1000) * 1000}
-          </h4>
-          <h4>Capital: {visibleCountries[0].capital}</h4>
+          <ul className="showCountryList">
+            <li>
+              Population:{" "}
+              {Math.round(visibleCountries[0].population / 1000) * 1000}
+            </li>
+            <li>Capital: {visibleCountries[0].capital}</li>
+          </ul>
           <img src={visibleCountries[0].flag} alt="" width="250" />
         </div>
       );
@@ -78,15 +81,14 @@ const App = () => {
 
   return (
     <main>
-      <div>
+      <div className="header">
         <h1>Country Search</h1>
-        <h4>
-          Search: <input onChange={onSearchValueChanged} />
-        </h4>
+        <div className="search">
+          <input onChange={onSearchValueChanged} className="searchBar" />
+        </div>
       </div>
       <div>
         <ul className="coutryList">{showCountries(visibleCountries)}</ul>
-
         <div>{showCountryData()}</div>
       </div>
     </main>
