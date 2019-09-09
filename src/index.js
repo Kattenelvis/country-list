98e3f43157fa99a0892c3394edd838a04f694273
Kattenelvis <officialkattenelvis@gmail.com>
2019-09-09 16:13:23 +0200

    Added sass

--- src/index.js
+++ src/index.js
@@ -1,81 +1,96 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios'
-import './index.css'
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./css/style.css";
+import ReactDOM from "react-dom";
 
-const countryList = [249]
-let currentSearchTerm = ''
+const countryList = [249];
+let currentSearchTerm = "";
 
+const App = () => {
+  const showCountries = countryArray =>
+    countryArray.map(country => (
+      <li key={country.numericCode} className="countryLi">
+        <button
+          className="btn"
+          value={country.name}
+          onClick={onSearchValueChanged}
+        >
+          Show
+        </button>{" "}
+        {country.name}
+      </li>
+    ));
 
+  const [visibleCountries, setVisibleCountries] = useState([]);
 
-const App = () =>{
+  const fetchCountries = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response => {
+      countryList.fill(response.data);
+      setVisibleCountries(response.data);
+    });
+  };
 
-    const showCountries = (countryArray) => countryArray.map(country => 
-        <li key={country.numericCode} className="countryLi">
-            <button className="btn" value={country.name}
-            onClick={onSearchValueChanged}>Show</button> {country.name} 
-        </li>)
-        
-    const [visibleCountries, setVisibleCountries] = useState([])
+  useEffect(fetchCountries, []);
 
-    const fetchCountries = () => {
-        axios
-        .get('https://restcountries.eu/rest/v2/all')
-        .then(response => {
-            countryList.fill(response.data)
-            setVisibleCountries(response.data)
-        })
+  const onSearchValueChanged = event => {
+    currentSearchTerm = event.target.value;
+    filterCountries();
+  };
+
+  const showCountryData = () => {
+    if (visibleCountries.length === 1) {
+      return (
+        <div>
+          <h1>{visibleCountries[0].name}</h1>
+          <h4>
+            Population:{" "}
+            {Math.round(visibleCountries[0].population / 1000) * 1000}
+          </h4>
+          <h4>Capital: {visibleCountries[0].capital}</h4>
+          <img src={visibleCountries[0].flag} alt="" width="250" />
+        </div>
+      );
     }
+  };
 
-    useEffect(fetchCountries, [])
+  const filterCountries = () => {
+    setVisibleCountries(
+      countryList[0].filter(
+        country =>
+          country.name
+            .toLowerCase()
+            .includes(currentSearchTerm.toLowerCase()) ||
+          country.region
+            .toLowerCase()
+            .includes(currentSearchTerm.toLowerCase()) ||
+          country.subregion
+            .toLowerCase()
+            .includes(currentSearchTerm.toLowerCase()) ||
+          country.alpha2Code
+            .toLowerCase()
+            .includes(currentSearchTerm.toLowerCase()) ||
+          country.alpha3Code
+            .toLowerCase()
+            .includes(currentSearchTerm.toLowerCase())
+      )
+    );
+  };
 
-    const onSearchValueChanged = (event) =>{
-        currentSearchTerm = event.target.value
-        filterCountries()
-    }
+  return (
+    <main>
+      <div>
+        <h1>Country Search</h1>
+        <h4>
+          Search: <input onChange={onSearchValueChanged} />
+        </h4>
+      </div>
+      <div>
+        <ul className="coutryList">{showCountries(visibleCountries)}</ul>
 
-    const showCountryData = () => {
-        if (visibleCountries.length === 1){
-            return( 
-                <div>
-                    <h1>{visibleCountries[0].name}</h1>
-                    <h4>Population: {Math.round(visibleCountries[0].population/1000)*1000}</h4>
-                    <h4>Capital: {visibleCountries[0].capital}</h4>
-                    <img  src={visibleCountries[0].flag} alt="" width="250"/>
-                
-                </div>
-            )
-        }         
-    }
+        <div>{showCountryData()}</div>
+      </div>
+    </main>
+  );
+};
 
-    const filterCountries = () =>{
-        setVisibleCountries(countryList[0].filter(country =>
-            country.name.toLowerCase().includes(currentSearchTerm.toLowerCase()) ||
-            country.region.toLowerCase().includes(currentSearchTerm.toLowerCase()) ||
-            country.subregion.toLowerCase().includes(currentSearchTerm.toLowerCase()) ||
-            country.alpha2Code.toLowerCase().includes(currentSearchTerm.toLowerCase()) ||
-            country.alpha3Code.toLowerCase().includes(currentSearchTerm.toLowerCase())
-            ))
-        }
-
-    return(
-    
-        <main>            
-            <div>
-                <h1>Country Search</h1>
-                <h4>Search:  <input onChange={onSearchValueChanged}/></h4> 
-            </div>
-            <div>
-                <ul className="coutryList">
-                    {showCountries(visibleCountries)}
-                </ul>
-
-                <div>{showCountryData()}</div>
-
-            </div>
-        </main>
-
-    )}
-
-ReactDOM.render(
-    <App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
